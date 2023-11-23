a6fce3d848f08423efb9ad268ad5507004d766ea
FarelDanendra <147696558+FarelDanendra@users.noreply.github.com>
2023-11-23 14:55:20 +0700

    Pertemuan tgl 23 Nop 2023

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,68 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Company Profile</title>
-    <!-- Tambahkan link ke Bootstrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-      crossorigin="anonymous"
-    />
-    <!-- Tambahkan link ke Google Fonts untuk Nunito -->
-    <link
-      href="https://fonts.googleapis.com/css?family=Nunito:400,700"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <div class="header">
-      <!-- Tambahkan logo di pojok kanan atas -->
-      <img src="logo_kecil.png" alt="Company Logo" class="logo" />
-    </div>
-    <div class="container">
-      <p class="main-title">The competition</p>
+<html>
+<head>
+	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
+</head>
+<body>
 
-      <!-- Row 1 -->
-      <div class="row">
-        <div class="col-md-4">
-          <p class="sub-title">Company 1</p>
-          <p class="sub-content">
-            their reviews average 3.5 <br />whereas we average 5 starts
-          </p>
-        </div>
-        <div class="col-md-4">
-          <p class="sub-title">Company 2</p>
-          <p class="sub-content">
-            their reviews average 3.5 <br />whereas we average 5 starts
-          </p>
-        </div>
-      </div>
+	<h2>www.malasngoding.com</h2>
+	<h3>Data Pegawai</h3>
 
-      <!-- Row 2 -->
-      <div class="row">
-        <div class="col-md-4">
-          <p class="sub-title">Company 3</p>
-          <p class="sub-content">
-            their reviews average 3.5 <br />whereas we average 5 starts
-          </p>
-        </div>
-        <div class="col-md-4">
-          <p class="sub-title">Company 4</p>
-          <p class="sub-content">
-            their reviews average 3.5 <br />whereas we average 5 starts
-          </p>
-        </div>
-      </div>
-    </div>
-    <img src="foto.png" alt="Company Image" class="gambar-bawah" />
-    <!-- ... (kode setelahnya) ... -->
+	<a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
 
-    <!-- Tambahkan link ke Bootstrap JavaScript (Popper.js dan jQuery) dan Bootstrap JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.min.js"></script>
-  </body>
+	<br/>
+	<br/>
+
+	<table border="1">
+		<tr>
+			<th>Nama</th>
+			<th>Jabatan</th>
+			<th>Umur</th>
+			<th>Alamat</th>
+			<th>Opsi</th>
+		</tr>
+		@foreach($pegawai as $p)
+		<tr>
+			<td>{{ $p->pegawai_nama }}</td>
+			<td>{{ $p->pegawai_jabatan }}</td>
+			<td>{{ $p->pegawai_umur }}</td>
+			<td>{{ $p->pegawai_alamat }}</td>
+			<td>
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
+				|
+				<a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
+			</td>
+		</tr>
+		@endforeach
+	</table>
+
+
+</body>
 </html>
